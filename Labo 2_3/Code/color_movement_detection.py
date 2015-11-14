d0224dff310d00ed092c2f8e7347248dc7a6818d
xserra93 <x.serra93@gmail.com>
2015-11-14 23:11:05 +0100

    Improved version

--- Labo 2_3/Code/color_movement_detection.py
+++ Labo 2_3/Code/color_movement_detection.py
@@ -14,39 +14,46 @@
 from collections import deque
 import numpy as np
 import cv2
+import time
 import math
+
+
+
+###########
+# METHODS #
+###########
 
 #Convenience resize function
 def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and
     # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
+	dim = None
+	(h, w) = image.shape[:2]
 
     # if both the width and height are None, then return the
     # original image
-    if width is None and height is None:
-        return image
+	if width is None and height is None:
+		return image
 
     # check to see if the width is None
-    if width is None:
+	if width is None:
         # calculate the ratio of the height and construct the
         # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
+		r = height / float(h)
+		dim = (int(w * r), height)
 
     # otherwise, the height is None
-    else:
+	else:
         # calculate the ratio of the width and construct the
         # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
+		r = width / float(w)
+		dim = (width, int(h * r))
 
     # resize the image
-    resized = cv2.resize(image, dim, interpolation=inter)
+	resized = cv2.resize(image, dim, interpolation=inter)
 
     # return the resized image
-    return resized
+	return resized
 
 
 def distance(list_x,list_y):
@@ -56,20 +63,88 @@ def distance(list_x,list_y):
 	return math.sqrt(return_value)
 
 
+def setup_trackbars(range_filter):
+	cv2.namedWindow("Trackbars", 0)
+	for i in ["MIN", "MAX"]:
+		for j in range_filter:
+			v = 0 if i == "MIN" else 255
+			cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, lambda x : None)
+
+
+def get_trackbar_values(range_filter):
+	values = {}
+
+	for i in ["MIN", "MAX"]:
+		for j in range_filter:
+			v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
+			values["%s_%s" % (j, i)] = v
+	return values
+
+
+##########
+# SCRIPT #
+##########
+
 # Set the camera
 VIDEODEV = 0
+
+
+##############################################
+# Identify the color of the objects to track #
+##############################################
+
 camera = cv2.VideoCapture(VIDEODEV); assert camera.isOpened()
+setup_trackbars('HSV')
+while True:
+        (grabbed, frame) = camera.read()
+        frame_to_thresh = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        v = get_trackbar_values('HSV')
+        thresh = cv2.inRange(frame_to_thresh, (v['H_MIN'], v['S_MIN'], v['V_MIN']), (v['H_MAX'], v['S_MAX'], v['V_MAX']))
+        cv2.imshow("Original", frame)
+        cv2.imshow("Thresh", thresh)
+        if cv2.waitKey(1) & 0xFF is ord('q'):
+            break
+            
+# Cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
+
+colorLower = (v['H_MIN'], v['S_MIN'], v['V_MIN'])
+colorUpper = (v['H_MAX'], v['S_MAX'], v['V_MAX'])
 
 
-############################
-# Set the background image #
-############################
-(grabbed, frame) = camera.read()
 
-# resize the frame, convert it to grayscale, and blur it
-frame = resize(frame, width=600)
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+###############################
+# Select the background image #
+###############################
+camera = cv2.VideoCapture(VIDEODEV); assert camera.isOpened()
+time.sleep(0.25)
+firstFrame = None
+
+# Loop over the frames of the video
+while True:
+    # Grab the current frame and initialize the occupied/unoccupied
+    # text
+    (grabbed, frame) = camera.read()
+    text = "Unoccupied"
+ 
+    # Resize the frame, convert it to grayscale, and blur it
+    frame = resize(frame, width=600)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # Show the frame and record if the user presses a key
+    cv2.imshow("Security Feed", gray)
+    key = cv2.waitKey(1) & 0xFF
+    # If the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+    
+first_frame = gray
+camera.release()
+cv2.destroyAllWindows()
+
 
 
 
@@ -83,35 +158,39 @@ colorUpper = (136,255,175)
 #colorLower = (31,98,10)
 #colorUpper = (122,255,121)
 objects_data = []		# Coordinates of every object to track
-num_elements = 2		# Number of objects to track
+num_elements = 4		# Number of objects to track
 mask_list = deque()		# Queue of masks to be able to track moving objects
-first_frame = gray		# Background image
 diffDelta = 20			# Threshold to determine when an object is moving
 min_distance = 100		# Minimum distance between objects to classify them as the same
 length_mask_queue = 5	# Length of mask queue
 
 
+
+
+
 ################
 # Keep looping #
 ################
+camera = cv2.VideoCapture(VIDEODEV); assert camera.isOpened()
+
 while True:
-	# grab the current frame
+	# Grab the current frame
 	(grabbed, frame) = camera.read()
     
-	# resize the frame, blur it, and convert it to the HSV
+	# Resize the frame, blur it, and convert it to the HSV
 	# color space
 	frame = resize(frame, width=600)
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 
-    # compute the absolute difference between the current frame and
+    # Compute the absolute difference between the current frame and
     # first frame
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
  	
 
-    # construct a mask for the color selected, then perform
+    # Construct a mask for the color selected, then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
 	mask = cv2.inRange(hsv, colorLower, colorUpper)
@@ -137,14 +216,14 @@ while True:
 		cv2.imshow("Mask", mask)
 
 
-	    # find contours in the mask and initialize the current
+	    # Find contours in the mask and initialize the current
 	    # (x, y) center of the ball
 		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
 		center = None
 
-	    # only proceed if at least one contour was found
+	    # Only proceed if at least one contour was found
 		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
+			# Find the largest contour in the mask, then use
 			# it to compute the minimum enclosing circle and
 			# centroid
 			c_maxes_aux = sorted(cnts, key=cv2.contourArea, reverse=True)
@@ -160,8 +239,7 @@ while True:
 					count = count + 1
 					((x, y), radius) = cv2.minEnclosingCircle(cmax)
 					found = False
-					# only proceed if the radius meets a minimum size
-					print cv2.contourArea(cmax)
+					# Only proceed if the radius meets a minimum size
 					for i in range(0,len(objects_data)):
 						element = objects_data[i]
 						if distance(element[0],[x, y]) < min_distance:
@@ -170,16 +248,15 @@ while True:
 							objects_data[i] = ([x, y], radius)
 							M = cv2.moments(cmax)
 							center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-							# draw the minimum enclosing circle, yellow colour in BGR format(), thickness = 2
+							# Draw the minimum enclosing circle, yellow colour in BGR format(), thickness = 2
 							cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-				            # draw the centroid,radius 5, red colour in BGR format(), thikness < 0 meaning that the circle is filled.
+				            # Draw the centroid,radius 5, red colour in BGR format(), thikness < 0 meaning that the circle is filled.
 							cv2.circle(frame, center, 5, (0, 0, 255), -1)
 							cv2.putText(frame, "Centroid  x: {}, y: {}".format(center[0], center[1]),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
 										0.35, (0, 0, 255), 1)
 							cv2.putText(frame, "minEnclosing: x: {}, y: {}".format(int(x), int(y)),(10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX,
 										0.35, (0, 0, 255), 1)
 					if not(found) and len(objects_data) < num_elements:
-						print "AFEGIT!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 						objects_data.append(([x, y], radius))
 			#indexes_to_keep.sort()
 			#for i in range(len(objects_data)-1,-1,-1):
@@ -189,6 +266,7 @@ while True:
 
 	    # Show the frame to our screen
 		cv2.imshow("Frame", frame)
+
 	key = cv2.waitKey(1) & 0xFF
     
     # If the 'q' key is pressed, stop the loop
