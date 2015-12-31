db00ae66e7a67a6a833e85bc256c06d51da982e4
xserra93 <x.serra93@gmail.com>
2015-12-31 11:48:01 +0100

    Labos 4.* started

---
+++ Labo 4_1/CV_Face_detection_1/FD_ex2.m
@@ -0,0 +1,14 @@
+% Computational Vision
+% 2015-2016
+% Student name: ...
+%
+% >> OBJECTIVE: 
+% 1) Write the code for Exercise 2
+% 2) Execute the code and check the results
+% 3) Comment the experiments and results in a report
+
+% main function
+function FD_ex2()
+
+
+end
---
+++ Labo 4_1/CV_Face_detection_2/FD_ex3.m
@@ -0,0 +1,29 @@
+% Computational Vision
+% 2015-2016
+% Student name: ...
+%
+% >> OBJECTIVE:
+% 1) Write the code for Exercise 3
+% 2) Execute the code and check the results
+% 3) Comment the experiments and results in a report
+
+% main
+function FD_ex3()
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
